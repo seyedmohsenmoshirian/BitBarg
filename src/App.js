8cf7c056207e43744b9d0cab119320d00ca6323d
Your Name <you@example.com>
2022-01-19 11:09:43 +0330

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import BITbarg from"./BITbarg.png"
+import callcenter from"./callcenter.png"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return <div className="App">
+    <img src={BITbarg} alt="Snow" className="bitbarg"/>
+    <div class="lll">
+        <button className="kkr">بیت کوین</button>
+        <button className="kkr">1</button>
+        <button className="kkr">1,191,982,698 </button>
     </div>
-  );
+    <div class="poi">انتخاب ارز</div>
+    <div class="poione">واحد </div>
+    <div class="poitwo">تومان </div>
+    <h1 contenteditable data-heading="  روز هفته ، ۲۴ ساعت شبانه‌روز
+    
+در خدمت شما هستیم ">
+
+
+
+روز هفته ، ۲۴ ساعت شبانه‌روز        
+در خدمت شما هستیم       
+
+
+
+</h1>
+<div class="ghj">91079667 </div>
+<div class="cirrcle">
+<img src={callcenter} alt="Snow" class="callcenter"/>
+    </div>
+    <button class="kktwo">ورود </button>
+
+<button class="kk">درخواست خرید</button>
+<button class="kkone">فروش</button>
+  </div>
 }
 
 export default App;
