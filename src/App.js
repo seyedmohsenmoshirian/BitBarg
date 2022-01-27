e9a65a7b9f6f0e58a34039151a9408da5396af4d
Your Name <you@example.com>
2022-01-27 22:27:16 +0330

    commit ss

--- src/App.js
+++ src/App.js
@@ -1,19 +1,65 @@
 import './App.css';
 import BITbarg from"./BITbarg.png"
-import callcenter from"./callcenter.png"
+import fff from"./fff.png"
+import rrr from"./rrr.png"
+
+// import hh from"./hh.svg"
+
+
 
 function App() {
   return <div className="App">
+
+    <div className="containerone">
+    <img src={rrr} alt="hh" className="hhone"/>
+    <button class="kktwo">ورود </button>
+    </div>
+
+
+<div className="containertwo">
     <img src={BITbarg} alt="Snow" className="bitbarg"/>
-    <div class="lll">
+    <div className="vvv">
+        <div className="qwer">
+            <button className="kkr">بیت کوین</button>
+            <div class="poi">انتخاب ارز</div>
+        </div>
+            <div className="qwer">
+                <button className="kkr">1</button>
+            <div className="poione">واحد </div>
+        </div>
+        <div className="qwer">
+            <button className="kkr">1,191,982,698 </button>
+            <div class="poitwo">تومان </div>
+        </div>
+<div className="bnmj">
+    <button class="kkone">فروش</button>
+<button class="kk">درخواست خرید</button>
+</div>
+<div className="xdrt"><h1 contenteditable data-heading="  روز هفته ، ۲۴ ساعت شبانه‌روز
+    
+    در خدمت شما هستیم ">
+    
+    
+    
+    روز هفته ، ۲۴ ساعت شبانه‌روز        
+    در خدمت شما هستیم       
+    
+    
+    
+    </h1></div>
+<div className="tyui"><div className="ghj">91079667 </div></div>
+    </div>
+    
+
+    </div>
+
+
+    {/* <div class="lll">
         <button className="kkr">بیت کوین</button>
         <button className="kkr">1</button>
         <button className="kkr">1,191,982,698 </button>
-    </div>
-    <div class="poi">انتخاب ارز</div>
-    <div class="poione">واحد </div>
-    <div class="poitwo">تومان </div>
-    <h1 contenteditable data-heading="  روز هفته ، ۲۴ ساعت شبانه‌روز
+    </div> */}
+    {/* <h1 contenteditable data-heading="  روز هفته ، ۲۴ ساعت شبانه‌روز
     
 در خدمت شما هستیم ">
 
@@ -24,16 +70,18 @@ function App() {
 
 
 
-</h1>
-<div class="ghj">91079667 </div>
+</h1> */}
+{/* <div class="ghj">91079667 </div> */}
 <div class="cirrcle">
-<img src={callcenter} alt="Snow" class="callcenter"/>
+<img src={fff} alt="Snow" class="callcenter"/>
     </div>
-    <button class="kktwo">ورود </button>
 
-<button class="kk">درخواست خرید</button>
-<button class="kkone">فروش</button>
+{/* <button class="kk">درخواست خرید</button>
+<button class="kkone">فروش</button> */}
   </div>
 }
 
 export default App;
+
+
+
